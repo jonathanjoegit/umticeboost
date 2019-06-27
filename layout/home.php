3f42adcf1017a63e50bb85c44170118309e088b3
jjupin <jonathan.jupin@univ-lemans.fr>
2019-06-27 16:15:38 +0200

    - bug navdrawer sur chrome (home page)

--- layout/home.php
+++ layout/home.php
@@ -27,16 +27,9 @@ defined('MOODLE_INTERNAL') || die();
 user_preference_allow_ajax_update('drawer-open-nav', PARAM_ALPHA);
 require_once($CFG->libdir . '/behat/lib.php');
 
-if (isloggedin()) {
-    $navdraweropen = (get_user_preferences('drawer-open-nav', 'true') == 'true');
-} else {
-    $navdraweropen = false;
-}
-$extraclasses = [];
-if ($navdraweropen) {
-    $extraclasses[] = 'drawer-open-left';
-}
-$bodyattributes = $OUTPUT->body_attributes($extraclasses);
+$navdraweropen = false;
+
+$bodyattributes = $OUTPUT->body_attributes();
 $blockshtml = $OUTPUT->blocks('side-pre');
 $hasblocks = strpos($blockshtml, 'data-block=') !== false;
 $regionmainsettingsmenu = $OUTPUT->region_main_settings_menu();
